a38014d2238cb4e8c08e67093ec91fc0474a9ec9
Benjamin <aeplibenjamin@gmail.com>
2024-08-19 17:52:55 +0200

    routing - first steps

--- Webapp/src/Routing/Router.php
+++ Webapp/src/Routing/Router.php
@@ -6,6 +6,33 @@ class Router {
 
     private array $routes = [];
 
-    
+    public function __construct() {
+        $this->routes = [
+            'POST' => [],
+            'PUT' => [],
+            'DELETE' => []
+        ];
+    }
 
+    public function post(string $path, string $action) {
+        $this->routes['POST'][$path] = $action;
+    }
+
+    public function put(string $path, string $action) {
+        $this->routes['PUT'][$path] = $action;
+    }
+
+    public function delete(string $path, string $action) {
+        $this->routes['DELETE'][$path] = $action;
+    }
+
+    public function dispatch(string $method, string $path) {
+        if (isset($this->routes[$method][$path])) {
+            $action = $this->routes[$method][$path];
+           
+            return $action; //pass routing to the controller
+        } else {
+            return '404 Not Found';
+        }
+    }
 }
