e427a2ba6c32c3541cd17671091641861bc04679
Benjamin <aeplibenjamin@gmail.com>
2024-08-19 17:52:24 +0200

    Add User Modell

---
+++ Webapp/src/Model/UserModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Webapp\src\Model;
+
+class UserModel
+{
+    public int $id;
+    public string $vorname;
+    public string $nachname;
+    public bool $istAdmin;
+    public string $nutzername;
+    public string $passwort;
+
+    public function __construct(int $id, string $vorname, string $nachname, bool $istAdmin, string $nutzername, string $passwort)
+    {
+        $this->id = $id;
+        $this->vorname = $vorname;
+        $this->nachname = $nachname;
+        $this->istAdmin = $istAdmin;
+        $this->nutzername = $nutzername;
+        $this->passwort = $passwort;
+    }
+}
