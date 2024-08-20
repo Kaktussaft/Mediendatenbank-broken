29a414973a5cad57d131167b4022085786919ebf
Benjamin <aeplibenjamin@gmail.com>
2024-08-20 11:12:17 +0200

    routes

--- Webapp/src/Routing/Index.php
+++ Webapp/src/Routing/Index.php
@@ -2,6 +2,7 @@
 
 require 'Router.php';
 require 'UserController.php';
+require 'MediumController.php';
 
 use Webapp\src\Controller\UserController;
 use Webapp\src\Controller\MediumController;
@@ -18,9 +19,19 @@ $router->addRoute("POST", "/Login", [$userController, "loginView"]);
 $router->addRoute("POST", "/User", [$userController, "logout"]);
 $router->addRoute("POST", "/Admin", [$userController, "adminView"]);
 
+$router->addRoute("DELETE", "/USER", [$mediumController, "deleteMedium"]);
+$router->addRoute("DELETE", "/ADMIN", [$userController, "deleteUser"]); //not a requirement, implementation for testing purposes
+
+$router->addRoute("PUT", "/USER", [$mediumController, "updateMedium"]);
+$router->addRoute("PUT", "/USER", [$mediumController, "uploadMedium"]);
+$router->addRoute("PUT", "/ADMIN", [$mediumController, "updateKeywords"]);
+$router->addRoute("PUT", "/ADMIN", [$userController, "updateUser"]); //not a requirement, implementation for testing purposes
 
 $method = $_SERVER['REQUEST_METHOD'];
 $path = $_SERVER['REQUEST_URI'];
 
 $response = $router->dispatch($method, $path);
 echo $response;
+
+?>
+
