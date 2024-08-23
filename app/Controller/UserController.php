544f85ef7427afb3e09ddb11b7444387b14b4c08
Benjamin <aeplibenjamin@gmail.com>
2024-08-23 13:49:59 +0200

    add register

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -11,4 +11,9 @@ class UserController extends Controller
             $this->view('LandingPage', 'Username not found');
         }
     }
+    public function register()
+    {
+        
+        $this->view('LandingPage', 'Registration successful');
+    }
 }
