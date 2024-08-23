92f61fe93cb086d7b9806ab0fa211c4479fba600
Benjamin <aeplibenjamin@gmail.com>
2024-08-23 17:12:50 +0200

    new shared file

---
+++ app/View/script/SharedScripts.js
@@ -0,0 +1,18 @@
+function test(string){
+    window.alert(string)
+}
+function test3(string){
+    window.alert(string)
+}
+function test(string){
+    window.alert(string)
+}
+function test2(string){
+    window.alert(string)
+}
+function test4(string){
+    window.alert(string)
+}
+function test5(string){
+    window.alert(string)
+}
