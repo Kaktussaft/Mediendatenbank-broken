5a101e2000c2500d176773eab049336a9400a4ba
Benjamin <aeplibenjamin@gmail.com>
2024-08-24 20:24:31 +0200

    toggle admin view place holder

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -7,13 +7,24 @@ class UserController extends Controller
         if ($username == '1234') {
             $this->view('User', ['username' => $username]);
         }
-        else{
-            $this->view('LandingPage', 'Username not found');
+        else{          
+            header('Location: http://localhost/Mediendatenbank/public/');
+            exit();
         }
     }
     public function register()
     {
-        
+
         $this->view('LandingPage', 'Registration successful');
+    }
+    public function toggleAdminView($username = '')
+    {
+        if ($username == '1234') {
+            $this->view('Admin', ['username' => $username]);
+        }
+        else{          
+            header('Location: http://localhost/Mediendatenbank/public/');
+            exit();
+        }
     }
 }
--- app/Controller/home.php
+++ app/Controller/home.php
@@ -4,9 +4,6 @@ class Home extends Controller
 {
    public function index($name = '')   //calling in models
    {
-    //  $user = $this->model('UserModel');
-    //  $user->name = 'Alex';
-
      $this->view('LandingPage', ['name' => $name]);
    }
  
