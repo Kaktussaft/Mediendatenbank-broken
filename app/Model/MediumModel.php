e7677212b6c5870647912f656a5d9a66b6ffbf5f
Benjamin <aeplibenjamin@gmail.com>
2024-08-25 07:53:38 +0200

    add MediumModels

--- app/Model/MediumModel.php
+++ app/Model/MediumModel.php
@@ -1,22 +1,71 @@
 <?php
 
-namespace WEB42\src\Model;
+use DateTime;
 
-class MediumModell
+class MediumModel
 {
     public int $id;
     public string $title;
-    public string $mediaType;
+    public DateTime $uploadDate;
     public string $fileSize;
     public string $description;
 
-    public function __construct( int $id, string $title, string $mediaType, string $fileSize, string $description)
+    public function __construct( int $id, string $title, DateTime $uploadDate, string $fileSize, string $description)
     {
         $this->id = $id;
         $this->title = $title;
-        $this->mediaType = $mediaType;
+        $this->uploadDate = $uploadDate;
         $this->fileSize = $fileSize;
         $this->description = $description;
     }
-   
+}
+
+class PhotoModel extends MediumModel
+{
+    public int $resolution;
+
+    public function __construct(int $id, string $title, DateTime $uploadDate, string $fileSize, string $description, int $resolution)
+    {
+        parent::__construct($id, $title, $uploadDate, $fileSize, $description);
+        $this->resolution = $resolution;
+    }
+}
+
+class VideoModel extends MediumModel
+{
+    public int $duration;
+    public int $resolution;
+
+    public function __construct(int $id, string $title, DateTime $uploadDate, string $fileSize, string $description, int $duration, int $resolution)
+    {
+        parent::__construct($id, $title, $uploadDate, $fileSize, $description);
+        $this->duration = $duration;
+        $this->resolution = $resolution;
+    }
+}
+
+class AudioBook extends MediumModel
+{
+    public int $duration;
+    public string $speaker;
+
+    public function __construct(int $id, string $title, DateTime $uploadDate, string $fileSize, string $description, int $duration, string $speaker)
+    {
+        parent::__construct($id, $title, $uploadDate, $fileSize, $description);
+        $this->duration = $duration;
+        $this->speaker = $speaker;
+    }
+}
+
+class Ebook extends MediumModel
+{
+    public string $author;
+    public string $pageCount;
+
+    public function __construct(int $id, string $title, DateTime $uploadDate, string $fileSize, string $description, string $author, string $pageCount)
+    {
+        parent::__construct($id, $title, $uploadDate, $fileSize, $description);
+        $this->author = $author;
+        $this->pageCount = $pageCount;
+    }
 }
