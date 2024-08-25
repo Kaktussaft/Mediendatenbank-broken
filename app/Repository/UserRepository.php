e1a448adf375b9ca8be493889c12705a3bf92c32
Benjamin <aeplibenjamin@gmail.com>
2024-08-25 07:57:06 +0200

    UserRepo tbd functions

---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+class UserRepository{
+
+    public static function create($user){
+
+    }
+
+    public static function read($id){
+
+    }
+    public static function readAll(){
+
+    }
+    public static function update($user){
+
+    }
+    public static function delete($id){
+
+    }
+}
