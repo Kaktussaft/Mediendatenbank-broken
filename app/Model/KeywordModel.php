82173d4e05a281e68eb89f3ff43103720034dae0
Benjamin <aeplibenjamin@gmail.com>
2024-08-25 07:56:22 +0200

    Keyword model

---
+++ app/Model/KeywordModel.php
@@ -0,0 +1,13 @@
+<?php
+
+class KeywordModel
+{
+    public int $id;
+    public string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
