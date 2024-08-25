1331805955345885a7d24c05eb06ec0c322fd97f
Benjamin <aeplibenjamin@gmail.com>
2024-08-25 08:09:03 +0200

    add Medium repo functions tbd

---
+++ app/Repository/MediumRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+class MediumRepository{
+
+    public static function createPhotoMedium($photo){
+
+    }
+    public static function createVideoMedium($video){
+
+    }
+    public static function createAudioBookMedium($autioBook){
+
+    }
+    public static function createEbookMedium($ebook){
+
+    }
+    public static function deleteMedium(int $id, string $type){
+
+    }
+    public static function readMedium(int $id, string $type){
+
+    }
+    public static function updatePhoto($photo){
+
+    }
+    public static function updateVideo($video){
+
+    }
+    public static function updateAudioBook($audioBook){
+
+    }
+    public static function updateEbook($ebook){
+
+    }
+
+}
