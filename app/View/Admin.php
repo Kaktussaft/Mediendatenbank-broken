acc91358f454a6d07c158bbadeb2176a094d3709
yzapf <yann.zapf@web.de>
2024-08-25 10:42:43 +0200

    fixed modal generation and css implementation

--- app/View/Admin.php
+++ app/View/Admin.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    <script type="text/javascript" src="script/SharedScripts.js"></script>
-    <link rel="stylesheet" href="style/popups.css">
+    <script type="text/javascript" src="../app/View/script/SharedScripts.js"></script>
+    <link rel="stylesheet" href="../app/View/style/PopUps.css">
 </head>
 <body>
     <h1>Adminview</h1>
