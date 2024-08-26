b51a8ebb2d88686d9a96d339750143a0309af26a
Benjamin <aeplibenjamin@gmail.com>
2024-08-26 14:16:18 +0200

    registration frontend pass data

--- app/View/LandingPage.php
+++ app/View/LandingPage.php
@@ -12,7 +12,7 @@
 <body>
     <h1>Landing Page</h1>
     <div class="loginForm">
-        <form id="loginForm" action="UserController/login/" method="POST" onsubmit="updateFormLoginAction()">
+        <form id="loginForm" action="" method="POST" onsubmit="updateFormLoginAction()">
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" placeholder="Username" required>
             <input type="hidden" name="Routing" value="login">
@@ -27,32 +27,46 @@
         <div class="modal-content">
             <span class="close" id="close-registration-modal">&times;</span>
             <h2>Registrierung</h2>
-            <form>
+            <form id="registrationForm" action="" method="POST" onsubmit="submitRegistration()">
+                <label for="firstname">Vorname:</label>
+                <input type="text" id="name" name="firstname" required>
+                <br><br>
+                <label for="lastname">Nachname:</label>
+                <input type="text" id="surname" name="lastname" required>
+                <br>
                 <label for="username">Username:</label>
-                <input type="text" id="name" name="name" required>
+                <input type="text" id="username_registration" name="name" required>
                 <br>
                 <label for="email">E-Mail:</label>
                 <input type="text" id="email" name="email" required>
                 <br>
-                <label for="lastname">Nachname:</label>
-                <input type="text" id="lastname" name="lastname" required>
-                <br>
-                <label for="firstname">Vorname:</label>
-                <input type="text" id="firstname" name="firstname" required>
-                <br><br>
                 <button type="submit">Registrieren</button>
             </form>
         </div>
     </div>
 
     <script>
-        function updateFormLoginAction() {
+       function updateFormLoginAction() {
             var username = document.getElementById('username').value;
-            var form = document.getElementsByTagName('form')[0];
+            var form = document.getElementById('loginForm');
             form.action = 'UserController/login/' + username;
-        };
+            console.log(username);
+            return true; 
+        }
 
-       // initModal('registrationModal', 'open-registration-modal', 'close-registration-modal');
+        function submitRegistration() {
+            var name = document.getElementById('name').value;
+            var surname = document.getElementById('surname').value;
+            var username = document.getElementById('username_registration').value;
+            var email = document.getElementById('email').value;
+            var form = document.getElementById('registrationForm');
+            form.action = 'UserController/register/' + name + '/' + surname + '/' + username + '/' + email;
+            return true; 
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            initModal('registrationModal', 'open-registration-modal', 'close-registration-modal');
+        });
     </script>
 </body>
 
