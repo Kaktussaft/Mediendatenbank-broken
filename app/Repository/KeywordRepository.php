3462ded606d7b0bc2889d1460cf63c017974ce85
Benjamin <aeplibenjamin@gmail.com>
2024-08-26 14:15:55 +0200

    add keyword repo

---
+++ app/Repository/KeywordRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository;
+
+class KeywordRepository{
+
+    public static function createKeyword($keyword){
+
+    }
+    public static function deleteKeyword(int $id){
+
+    }
+    public static function readKeyword(int $id){
+
+    }
+    public static function updateKeyword($keyword){
+
+    }
+
+}
