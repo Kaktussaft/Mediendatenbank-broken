d87db5994f6619608913cf92a515e7cba1025c54
Benjamin <aeplibenjamin@gmail.com>
2024-08-26 14:15:27 +0200

    db-connection class

--- app/database/DbConnection.php
+++ app/database/DbConnection.php
@@ -2,24 +2,25 @@
 
 namespace App\Database;
 
-class Database{
+use PDO;
+use PDOException;
 
-public static $host = 'localhost';
-public static $dbName = 'WEB42';
-public static $username = 'root';
-public static $password = '';
+class Database
+{
 
-public static function connect() {
-    try {
-        $pdo = new PDO("mysql:host=".self::$host.";dbname=".self::$dbName.";charset=utf8", self::$username, self::$password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+    public static $host = 'localhost';
+    public static $dbName = 'WEB42';
+    public static $username = 'root';
+    public static $password = '';
+
+    public static function connect()
+    {
+        try {
+            $pdo = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$dbName . ";charset=utf8", self::$username, self::$password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
     }
 }
-
-
-
-}
-
