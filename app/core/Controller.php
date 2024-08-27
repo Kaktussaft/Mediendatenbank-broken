1e1f124e186d7bdee0e877a36c9f8ea7c8f78ab9
Benjamin <aeplibenjamin@gmail.com>
2024-08-27 15:21:33 +0200

    clean input

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,7 +4,7 @@ namespace App\Core;
 
 class Controller
 {
-    public function model ($model)
+    public function model($model)
     {
         require_once '../app/model/' . $model . '.php';
         return new $model();
@@ -12,7 +12,15 @@ class Controller
 
     public function view($view, $data = null)
     {
-       require_once '../app/view/'. $view . '.php'; 
+        require_once '../app/view/' . $view . '.php';
     }
     
+    function sanitizeUserInput($name, $surname, $username,  $email)
+    {
+        $name = (string) $name;
+        $surname = (string) $surname;
+        $username = (string) $username;
+        $email = (string) $email;
+        return [$name, $surname, $username, $email];
+    }
 }
