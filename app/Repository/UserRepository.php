48f0e1efaa0057665054710912578c533f9fe164
Benjamin <aeplibenjamin@gmail.com>
2024-08-27 15:21:47 +0200

    create, read, get

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -3,26 +3,57 @@
 namespace App\Repository;
 
 use App\Model\UserModel;
+use App\Database\DbConnection;
 
 
 
-class UserRepository{
-
-    //$this-> usermodel = new UserModel();
-
-    public static function createUser($user){
-
+class UserRepository
+{
+    private $conn;
+    public function __construct()
+    {
+        $this->conn = DbConnection::getInstance()->getConnection();
     }
 
-    public static function readUserByUsername($username){
-        //return false if not, return a user object if exists
-        return true;
-    }
-    public static function readAllUser(){
+    public function createUser($user)
+    {
+        $username = $user->getUsername();
+        $email = $user->getEmail();
+        $surname = $user->getSurname();
+        $name = $user->getName();
+        $isAdmin = $user->getIsAdmin();
+        $registrationDate = $user->getRegistrationDate()->format('Y-m-d'); // Format as date
 
+        $stmt = $this->conn->prepare("INSERT INTO Benutzer (Benutzername, EMail, Nachname, Vorname, Rolle, Registrierungsdatum) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $username, $email, $surname, $name, $isAdmin, $registrationDate);
+        $stmt->execute();
+        $stmt->close();
     }
-    public static function updateUser($user){
+    public function readUserByUsername($username)
+    {
+        $stmt = $this->conn->prepare("SELECT Benutzername FROM Benutzer WHERE Benutzername = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
+        if ($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    public function getUserByUsername($username)
+    {
+
+        $stmt = $this->conn->prepare("SELECT * FROM Benutzer WHERE Benutzername = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+        $user = $result->fetch_assoc();
+        $stmt->close();
+        return $user;
+    }
+    public function updateUser($user) {}
 }
